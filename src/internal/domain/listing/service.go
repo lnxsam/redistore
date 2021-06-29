8c8e0afbabb0ba7e3afeaf8a542ee060e67e5cb6
saman shahroudi <shahroudi.dev@gmail.com>
2021-06-29 22:07:05 +0430

    implement listing service

--- src/internal/domain/listing/service.go
+++ src/internal/domain/listing/service.go
@@ -3,8 +3,31 @@ package listing
 import (
 	"context"
 	"redistore/internal/domain"
+	"redistore/internal/domain/ports"
+	"redistore/pkg/yerror"
 )
 
 type Service interface {
 	GetProductList(ctx context.Context) ([]domain.Product, error)
+}
+
+func New(repo ports.Repository) Service {
+	return service{
+		repo: repo,
+	}
+}
+
+type service struct {
+	repo ports.Repository
+}
+
+func (s service) GetProductList(ctx context.Context) ([]domain.Product, error) {
+	const op yerror.Op = "domain.listing.service.GetProductList"
+
+	products, err := s.repo.GetProductList(ctx)
+
+	if err != nil {
+		return nil, yerror.E(op, err)
+	}
+	return products, nil
 }
