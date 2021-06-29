6f54d14c82778b34b7a721ec647a5a6ec5c9fe9c
saman shahroudi <shahroudi.dev@gmail.com>
2021-06-29 22:08:47 +0430

    implement searching service

--- src/internal/domain/searching/service.go
+++ src/internal/domain/searching/service.go
@@ -2,9 +2,37 @@ package searching
 
 import (
 	"context"
+	"errors"
 	"redistore/internal/domain"
+	"redistore/internal/domain/ports"
+	"redistore/pkg/yerror"
 )
 
 type Service interface {
 	SearchProductsByTitle(ctx context.Context, titleKeywords string) ([]domain.Product, error)
+}
+
+func New(repo ports.Repository) Service {
+	return service{
+		repo: repo,
+	}
+}
+
+type service struct {
+	repo ports.Repository
+}
+
+func (s service) SearchProductsByTitle(ctx context.Context, titleKeywords string) ([]domain.Product, error) {
+	const op yerror.Op = "domain.creating.service.SearchProductsByTitle"
+
+	if titleKeywords == "" {
+		return nil, yerror.E(op, yerror.KindInvalidArgument, errors.New("the titleKeywords is empty"))
+	}
+
+	products, err := s.repo.SearchProductsByTitle(ctx, titleKeywords)
+
+	if err != nil {
+		return nil, yerror.E(op, err)
+	}
+	return products, nil
 }
